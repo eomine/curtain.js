6a4771c6a2c0bb8015d44a75004304827c2dc0f5
Eduardo Omine <eduardo.omine@gmail.com>
2021-05-20 11:54:40 -0300

    Creates new empty curtain.ts

---
+++ src/curtain.spec.ts
@@ -0,0 +1,5 @@
+import Curtain from './curtain';
+
+test('Curtain should return null', () => {
+  expect(Curtain()).toEqual(null);
+});
---
+++ src/curtain.ts
@@ -0,0 +1,3 @@
+export function Curtain(): null {
+  return null;
+}
